6179651c2ae12a7947c7d04481e58db059291b83
Mateusz <mateuszkrolik87@gmail.com>
2023-05-24 06:53:40 +0200

    /opis

---
+++ Developer/branching.sh
@@ -0,0 +1,15 @@
+git commit 
+git branch
+git checkout
+git merge
+git rebase
+git checkout HEAD~4
+git branch -f main HEAD~3
+git branch -f bugFix HEAD~1 
+git reset
+git revert
+git cherry-pick
+git rebase -i HEAD~1
+git commit --amend
+git rebase caption main 
+git tag v1
